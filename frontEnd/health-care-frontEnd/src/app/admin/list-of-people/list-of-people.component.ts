687c013f10393702460ff80881b82d50216affc5
DavidNguyenHuu <37845640+DavidNguyenHuu@users.noreply.github.com>
2021-04-09 19:51:37 -0400

    Update list-of-people.component.ts

--- frontEnd/health-care-frontEnd/src/app/admin/list-of-people/list-of-people.component.ts
+++ frontEnd/health-care-frontEnd/src/app/admin/list-of-people/list-of-people.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
+export interface List{
+  name: string;
+  date_of_birth:string;
+}
 
 @Component({
   selector: 'app-list-of-people',
   templateUrl: './list-of-people.component.html',
   styleUrls: ['./list-of-people.component.css']
 })
-export class ListOfPeopleComponent implements OnInit {
+export class ListOfPeopleComponent implements AfterViewInit  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
+  listOfPeople: List[] = [
+    {name: '2020-04-06', date_of_birth: 'CHUM'},
+
+  ];
+
+  displayedColumns: string[] = ['First Name', 'Last Name','date of birth','Medicare Number'];
+  dataSource = new MatTableDataSource(this.listOfPeople);
+
+
+  // @ts-ignore
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+ 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
