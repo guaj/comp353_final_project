f77f93455da2e26b3cb6720d6d7bb5227218c772
Jasmin Guay <jasmin.guay@mail.concordia.ca>
2021-04-09 12:13:10 -0400

    Front

---
+++ frontEnd/health-care-frontEnd/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import { AdminComponent } from './admin/admin.component';
+import { TestHistoryComponent } from './main/test-history/test-history.component';
+import { AlertsComponent } from './main/alerts/alerts.component';
+import { PositiveTestFormsComponent } from './main/positive-test-forms/positive-test-forms.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatSidenavModule} from "@angular/material/sidenav";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    AdminComponent,
+    TestHistoryComponent,
+    AlertsComponent,
+    PositiveTestFormsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontEnd/health-care-frontEnd/src/app/main/positive-test-forms/positive-test-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positive-test-forms',
+  templateUrl: './positive-test-forms.component.html',
+  styleUrls: ['./positive-test-forms.component.css']
+})
+export class PositiveTestFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
